0f3b0c20fcab4fbbe9289e8f6dcf389da22fb642
TLEternal <109238149+01Eternal@users.noreply.github.com>
2025-03-23 18:58:30 -0300

    Criar ModClass_Register_System.js

---
+++ ModClass_Register_System.js
@@ -0,0 +1,28 @@
+let ModClass_Namespaces_Registered = {}
+
+function Register_Mod_Class(_Class_Name, _Class_Namespace, _class) {
+    let Has_Namespace = ModClass_Namespaces_Registered[_Class_Namespace]
+    if (!Has_Namespace) ModClass_Namespaces_Registered[_Class_Namespace] = [];
+    
+    ModClass_Namespaces_Registered[_Class_Namespace].push({ _Class_Name, _class });
+  }
+
+function using(_ModClass) {
+  if (ModClass_Namespaces_Registered[_ModClass]) {
+    ModClass_Namespaces_Registered[_ModClass].forEach(({ _Class_Name, _class }) => {
+      
+      return globalThis[_Class_Name] = _class;
+    });
+  } else console.log(`"${_ModClass}" no founded.`);
+}
+
+class Testing {
+  static Loggin() {
+    console.log('Logged')
+  }
+}
+
+Register_Mod_Class('Other_Name', 'ModType', Testing)
+using('ModType')
+
+Other_Name.Loggin() // Logged
